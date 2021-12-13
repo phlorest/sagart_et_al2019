6feb826d4e1abcd91dadd54f9847d38811c209f1
SimonGreenhill <simon@simon.net.nz>
2021-12-14 12:20:16 +1300

    update

--- cldfbench_sagart_et_al2019.py
+++ cldfbench_sagart_et_al2019.py
@@ -8,37 +8,25 @@ class Dataset(phlorest.Dataset):
     id = "sagart_et_al2019"
 
     def cmd_makecldf(self, args):
-        """
-summary.trees: original/sinotibetan-beast-covarion-relaxed-fbd.mcct.trees
-	nexus trees -t -c $< -o $@
-
-posterior.trees: original/sinotibetan-march-beast-covarion-relaxed-fbd.trees.gz
-	# remove 1000 (10%), sample 1000
-	nexus trees -c -d 1-1000 -n 1000 $< -o $@
-
-data.nex:
-	cp original/sino-tibetan-beastwords.nex $@
-        """
         self.init(args)
-        with self.nexus_summary() as nex:
-            self.add_tree_from_nexus(
-                args,
-                self.raw_dir / 'sinotibetan-beast-covarion-relaxed-fbd.mcct.trees',
-                nex,
-                'summary',
-                detranslate=True,
-            )
+        args.writer.add_summary(
+            self.raw_dir.read_tree(
+                'sinotibetan-beast-covarion-relaxed-fbd.mcct.trees', detranslate=True),
+            self.metadata,
+            args.log)
+
         posterior = self.sample(
             self.remove_burnin(
-                self.read_gzipped_text(
-                    self.raw_dir / 'sinotibetan-march-beast-covarion-relaxed-fbd.trees.gz'),
-                1000,
-            ),
+                self.raw_dir.read('sinotibetan-march-beast-covarion-relaxed-fbd.trees.gz'),
+                1000),
             detranslate=True,
             as_nexus=True)
-
-        with self.nexus_posterior() as nex:
-            for i, tree in enumerate(posterior.trees.trees, start=1):
-                self.add_tree(args, tree, nex, 'posterior-{}'.format(i))
-
-        self.add_data(args, self.raw_dir / 'sino-tibetan-beastwords.nex')
+        args.writer.add_posterior(
+            posterior.trees.trees, 
+            self.metadata, 
+            args.log)
+            
+        args.writer.add_data(
+            self.raw_dir.read_nexus('sino-tibetan-beastwords.nex'),
+            self.characters, 
+            args.log)
