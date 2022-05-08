71f1e77d3ab12da74db417785122f0cd7433dbd5
SimonGreenhill <simon@simon.net.nz>
2022-05-08 21:05:15 +1200

    update

--- cldfbench_sagart_et_al2019.py
+++ cldfbench_sagart_et_al2019.py
@@ -9,23 +9,19 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
+        
         args.writer.add_summary(
             self.raw_dir.read_tree(
-                'sinotibetan-beast-covarion-relaxed-fbd.mcct.trees', detranslate=True),
+                'sinotibetan-beast-covarion-relaxed-fbd.mcct.trees',
+                detranslate=True),
             self.metadata,
             args.log)
 
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('sinotibetan-march-beast-covarion-relaxed-fbd.trees.gz'),
-                1000),
-            detranslate=True,
-            as_nexus=True)
-        args.writer.add_posterior(
-            posterior.trees.trees, 
-            self.metadata, 
-            args.log)
-            
+        posterior = self.raw_dir.read_trees(
+            'sinotibetan-march-beast-covarion-relaxed-fbd.trees.gz',
+            burnin=1001, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
+
         args.writer.add_data(
             self.raw_dir.read_nexus('sino-tibetan-beastwords.nex'),
             self.characters, 
